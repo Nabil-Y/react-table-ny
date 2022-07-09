da29885a840e61bd11c09e4430bcaa68acec8bdc
nabil-y <nabil.yassine.dev@gmail.com>
2022-07-09 02:46:17 +0200

    Refactor app and configs for library

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import * as path from "path";
 import { defineConfig } from "vite";
+import libCss from "vite-plugin-libcss";
+import dts from "vite-dts";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts(), libCss()],
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.ts"),
+      name: "react-table-ny",
+      fileName: (format) => `${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+    sourcemap: true,
+    target: "esnext",
+    minify: false,
   },
 });
